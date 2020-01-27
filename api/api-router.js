18d9f720b2aa68641a29ac865159e2549732833d
Mr. Jonah <jaitch@gmail.com>
2020-01-27 17:17:29 -0600

    Add some middleware

--- api/api-router.js
+++ api/api-router.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
 const Users = require('../users/user-model');
+const restricted = require('../auth/restircted-middleware');
 
 router.get('/', (req, res) => {
     res.json({ api: "It's alive!"})
 });
 
-router.get('/users', (req, res) => {
+router.get('/users', restricted, (req, res) => {
     Users.find()
     .then(list => res.status(200).json(list))
     .catch(err => {
-        console.log(err)
         res.status(500).json({ error: 'Unable to get a list of users.'})
     })
 });
@@ -19,18 +19,14 @@ router.get('/users', (req, res) => {
 router.post('/register', (req, res) => {
     let newUser = req.body;
 
+
+    const hash = bcrypt.hashSync(newUser.password, 12)
+    newUser.password = hash;
+
     if(req.body) {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-            if(err){
-                res.status(500).json(err)
-            } else {
-                newUser.password = hash
-                Users.addUser(newUser)
-                .then(user => res.status(201).json(user))
-                .catch(err => {console.log(err); res.status(500).json({ error: 'There was an error adding a new user.'})})
-            }
-            
-        })
+        Users.addUser(newUser)
+        .then(user => res.status(201).json(user))
+        .catch(err => {console.log(err); res.status(500).json({ error: 'There was an error adding a new user.'})})    
     } else {
         res.status(500).json({ error: "missing user info"})
     }
@@ -39,23 +35,20 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
     let {username, password} = req.body;
-    Users.findUserBy({username}).first()
+    Users.findUserBy({username})
+    .first()
     .then(user => {
         if(user && bcrypt.compareSync(password, user.password)) {
             Users.find()
             .then(list => res.status(200).json(list))
-            .catch(err => res.status(401).json({ message: 'YOU SHALL NOT PASS!!!'}))
+            .catch(err => res.status(403).json({ message: 'YOU SHALL NOT PASS!!!'}))
         } else {
-            res.status(401).json({ message: 'Invalid Credentials.'})
+            res.status(403).json({ message: 'Invalid Credentials.'})
         }
     })
     .catch(err => {
         res.status(500).json(err)
     })
 })
-
-function hashMiddleware(req, res, next){
-
-}
 
 module.exports = router
---
+++ auth/restircted-middleware.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/user-model');
+
+module.exports = function restricted(req, res, next) {
+    const { username, password } = req.headers;
+
+    if(username && password) {
+        Users.findUserBy({username})
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)) {
+                next();
+            } else {
+                res.status(403).json({ message: 'Invalid Credentials.'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+    } else {
+        res.status(400).json({ message: 'Please provide valid credentials'});    
+    }
+}
