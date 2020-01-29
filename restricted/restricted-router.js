59ad1d26fb3fa79d839ac4ba77124ffa6a1ba488
Mr. Jonah <jaitch@gmail.com>
2020-01-28 18:06:49 -0600

    some restricted changes

--- restricted/restricted-router.js
+++ restricted/restricted-router.js
@@ -16,6 +16,7 @@ router.get('/other', restricted, (req, res) => {
 
 router.get('/a', restricted, (req, res) => {
     res.status(200).json({ message: 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'})
+    console.log(req.session.store)
 });
 
 module.exports = router;
